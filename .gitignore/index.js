24d7a4ac54343dfd7ced2ad22d1c07f90de2cd6e
R3nji <54752148+R3nji@users.noreply.github.com>
2019-09-01 17:56:17 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("En développement");
+    console.log("Le bot a bien été connecté");
+})
+
+bot.login('process.env.TOKEN');
